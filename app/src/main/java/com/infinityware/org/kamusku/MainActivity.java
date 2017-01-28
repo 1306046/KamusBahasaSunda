c65490409359d0c143e915ba042555098144154d
1306046 <1306046@sttgarut.ac.id>
2017-01-28 17:52:37 +0700

    KAMUS SUNDA INDONESIA

---
+++ app/src/main/java/com/infinityware/org/kamusku/MainActivity.java
@@ -0,0 +1,79 @@
+package com.infinityware.org.kamusku;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import com.infinityware.org.kamusku.AmbilData.JSONObjectResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    EntitasKamus kamus = new EntitasKamus();
+    ArrayList<EntitasKamus> listKamus = new ArrayList<EntitasKamus>();
+
+    EditText indo;
+    TextView sunda;
+    Button btTambah;
+    ImageButton btTrans;
+    //String url = "http://10.0.2.2/KamusAndroid/kamus.php"; //INI UNTUK Emulator
+    String url = "http://192.168.42.48/KamusAndroid/kamus.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        indo = (EditText) findViewById(R.id.input);
+        sunda = (TextView) findViewById(R.id.output);
+        btTambah = (Button) findViewById(R.id.bTambah);
+        btTrans = (ImageButton) findViewById(R.id.bSearch);
+
+        btTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, TambahActivity.class);
+                Bundle b = new Bundle();
+                b.putString("var","kosong");
+                in.putExtras(b);
+                startActivity(in);
+
+            }
+        });
+
+        btTrans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AmbilData ambilData = new AmbilData();
+                ambilData.init(indo.getText().toString(),url,MainActivity.this,jres);
+                indo.setText("");
+            }
+        });
+
+    }
+
+    JSONObjectResult jres = new JSONObjectResult() {
+        @Override
+        public void gotJSONObject(JSONObject jsonObject) {
+            try {
+                JSONArray jsonArray = jsonObject.getJSONArray("kamus");
+                for(int i=0; i<jsonArray.length(); i++){
+                    sunda.setText(jsonArray.getJSONObject(i).getString("sunda"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+}
